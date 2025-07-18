e4bc6000c5d790835adb8827137ac0c262654f1c
sdahal-dev <sdahal.contact@gmail.com>
2025-07-18 14:48:20 -0500

    Added the necessary files via upload
    📂 This project includes the core files for a JavaScript-powered calculator. Uploaded to showcase fundamental concepts like operators, functions, and DOM interaction—all put into practice through a clean, functional design.

---
+++ script.js
@@ -0,0 +1,16 @@
+const diaplay = Number(document.getElementById("display"));
+
+function showDisplay(input) {
+  display.value += input;
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "ERROR";
+  }
+}
+function clearDisplay() {
+  display.value = "";
+}
