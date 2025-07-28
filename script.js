953e1448ca4136bcc1f0ff43352bba245edb325a
sdahal-dev <sdahal.contact@gmail.com>
2025-07-27 20:41:19 -0500

    Added keyboard support

--- script.js
+++ script.js
@@ -14,3 +14,20 @@ function calculate() {
 function clearDisplay() {
   display.value = "";
 }
+
+document.addEventListener("keydown", (event) => {
+  const allowedKeys = "0123456789+-*/=^.";
+
+  if (allowedKeys.includes(event.key)) {
+    showDisplay(event.key === "^" ? "**" : event.key);
+  }
+  if (event.key === "=" || event.key === "Enter") {
+    calculate();
+    if (event.key === "Backspace") {
+      clearDisplay();
+    }
+  }
+  if (event.key === "Backspace") {
+    display.value = display.value.slice(0, -1);
+  }
+});
